9cc2c2d8c088bf5aec8644e6dc6b7a5f1ae432dc
Richie Wong <richiewong07@yahoo.com>
2018-01-21 14:17:04 -0600

    updated

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,18 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  return str.split('').every((char, i) => {
+    return char === str[str.length - i - 1]
+  });
+}
+
+console.log(palindrome('abba'));
 
 module.exports = palindrome;
+
+
+// function palindrome(str) {
+//   const reversed = str.split('').reverse().join('');
+//   return str === reversed;
+// }
