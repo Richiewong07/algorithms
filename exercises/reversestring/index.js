4826d03a96e0e29936d8890f2ef2ecaba6160087
Richie Wong <richiewong07@yahoo.com>
2018-01-21 11:35:52 -0600

    updated

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,21 +7,21 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-<<<<<<< HEAD
+
   let reversed = '';
   for (let character of str) {          // creates variable to loop through iterable object
     reversed = character + reversed;
   }
   return reversed;
 }
-=======
+
   const arr = str.split('');    // turns string to array
   arr.reverse();                // reverse element in array
   return arr.join('');          // joins back into string
 }
 
 console.log(reverse('apple'));
->>>>>>> 1183469c6a933b35213c6617ef47020467ae440c
+
 
 module.exports = reverse;
 
