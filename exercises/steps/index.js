ffd3194c6f970096f18d60f9ade882b3ec74da43
Richie Wong <richiewong07@yahoo.com>
2018-01-28 18:56:31 -0600

    update

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,8 +17,23 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  
+function steps(n, row = 0, stair = '') {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    steps(n, row + 1);
+    return;
+  }
+
+  if (stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+  steps(n, row, stair);
 }
 
 console.log(steps(4))
