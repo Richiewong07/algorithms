770b74ca4b685aef6a60580ea8cec2f425f4cd2f
Richie Wong <richiewong07@yahoo.com>
2018-01-29 16:38:44 -0600

    update

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,24 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi)    // reg ex expression; g : don't stop at first match; i : case insensitive
+  return matches ? matches.length : 0;    // match gives an array so need to return length of the array
+}
+
+console.log(vowels('Hi There!'));
 
 module.exports = vowels;
+
+// function vowels(str) {
+//   let count = 0;
+//   const checker = ['a', 'e', 'i', 'o', 'u'];
+//
+//   for (let char of str.toLowerCase()) {
+//     if (checker.includes(char)) {   // check if char in string is a vowel
+//       count++;
+//     }
+//   }
+//
+//   return count;
+// }
