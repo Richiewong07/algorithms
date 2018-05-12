01a379f89775b004ff3f6b415b1f6926692c35b8
Richie Wong <richiewong07@yahoo.com>
2018-05-11 21:14:42 -0500

    update

--- Python/algorithm_5.py
+++ Python/algorithm_5.py
@@ -2,4 +2,28 @@
 #
 # What is the 10,001st prime number?
 
-if num
+
+def is_prime(n):
+    # look for factors of 2 first
+    if n % 2 == 0: return False
+    # now look for odd factors
+    p = 3
+    while p < n**0.5+1:
+        if n % p == 0: return False
+        p += 2
+    return True
+
+def nth_prime(n):
+    prime = 2
+    count = 1
+    iter = 3
+    while count < n:
+        if is_prime(iter):
+            prime = iter
+            count += 1
+        iter += 2
+    print(prime)
+
+
+n = 10001
+nth_prime(n)
