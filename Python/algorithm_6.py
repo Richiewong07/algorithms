53008074ba5dfc6f4eecc8a0ab1ad5728e9f2e13
Richie Wong <richiewong07@yahoo.com>
2018-05-13 19:51:31 -0500

    new

---
+++ Python/algorithm_6.py
@@ -0,0 +1,5 @@
+# The Collatz Conjecture goes like this:
+# 
+# Take any number n. If n is even, divide it by 2, if n is odd, multiply it by 3 and add 1. Repeat the process indefinitely, and you'll eventually reach 1.
+# 
+# Given a num variable, can you print all the numbers in num's Collatz sequence until 1 is reached? Print the numbers space-separated (and each test case on its own line).
